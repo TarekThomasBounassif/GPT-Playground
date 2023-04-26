877af1caaf0ba7b4cc5fbd40af2e5cc7e97550b4
TarekThomasBounassif <89601736+TarekThomasBounassif@users.noreply.github.com>
2023-04-26 21:26:43 +0200

    Added md file, tested subject extraction with toekization

--- text_cleaner.py
+++ text_cleaner.py
@@ -1,15 +1,24 @@
 
 import nltk
+from nltk.stem import PorterStemmer
 
-# download the stop words list (only needed once)
-nltk.download('stopwords')
-
-# import the stop words list
-stop_words = nltk.corpus.stopwords.words('english')
-
-print(stop_words)
 class TextCleaner:
 
-
     def __init__(self) -> None:
-        pass
+        nltk.download('stopwords')
+        self.stop_words = nltk.corpus.stopwords.words('english')
+        self.stemmer = PorterStemmer()
+
+    def remove_stop_words_string(self, str_in:str) -> str:
+        for s_word in self.stop_words:
+            str_in = str_in.replace(s_word, '')
+        return str_in
+    
+    def tokenize(self, str_in:str) -> list:
+        str_in = str_in.replace('\n', '').replace('\t', '')
+        tokens = str_in.split()
+        tokens = [token for token in tokens if len(token) > 1]
+        tokens = [token.lower() for token in tokens]
+        tokens = [token for token in tokens if token not in self.stop_words]
+        #tokens = [self.stemmer.stem(token) for token in tokens]
+        return tokens
